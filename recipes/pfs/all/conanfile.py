53a7e313c1ede01150345f85d1c7bba0c34f3bc8
Andreas Kiefer <kieferandreas1@gmail.com>
2024-09-28 11:46:29 +0200

    Add pfs version 0.10.0

--- recipes/pfs/all/conanfile.py
+++ recipes/pfs/all/conanfile.py
@@ -16,45 +16,36 @@ required_conan_version = ">=1.53.0"
 #
 
 
-class PackageConan(ConanFile):
-    name = "package"
-    description = "short description"
-    # Use short name only, conform to SPDX License List: https://spdx.org/licenses/
-    # In case not listed there, use "LicenseRef-<license-file-name>"
-    license = ""
+class PfsConan(ConanFile):
+    name = "pfs"
+    description = "procfs parsing library in C++"
+    license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
-    homepage = "https://github.com/project/package"
-    # no "conan" and project name in topics. Use topics from the upstream listed on GH
-    topics = ("topic1", "topic2", "topic3")
-    # package_type should usually be "library" (if there is shared option)
+    homepage = "https://github.com/dtrugman/pfs"
+    topics = ("linux", "programming", "cpp", "procfs", "cpp-library")
     package_type = "library"
     settings = "os", "arch", "compiler", "build_type"
     options = {
         "shared": [True, False],
         "fPIC": [True, False],
+        "asan": [True, False],
+        "build_samples": [True, False],
+        "build_tests": [True, False],
+        "coverage": [True, False],
     }
     default_options = {
         "shared": False,
         "fPIC": True,
+        "asan": False,
+        "build_samples": True,
+        "build_tests": True,
+        "coverage": False,
     }
 
     @property
     def _min_cppstd(self):
-        return 14
+        return 11
 
-    # in case the project requires C++14/17/20/... the minimum compiler version should be listed
-    @property
-    def _compilers_minimum_version(self):
-        return {
-            "apple-clang": "10",
-            "clang": "7",
-            "gcc": "7",
-            "msvc": "191",
-            "Visual Studio": "15",
-        }
-
-    # no exports_sources attribute, but export_sources(self) method instead
-    # this allows finer grain exportation of patches per version
     def export_sources(self):
         export_conandata_patches(self)
 
@@ -65,67 +56,39 @@ class PackageConan(ConanFile):
     def configure(self):
         if self.options.shared:
             self.options.rm_safe("fPIC")
-        # for plain C projects only
-        self.settings.rm_safe("compiler.cppstd")
-        self.settings.rm_safe("compiler.libcxx")
 
     def layout(self):
-        # src_folder must use the same source folder name the project
         cmake_layout(self, src_folder="src")
 
-    def requirements(self):
-        # prefer self.requires method instead of requires attribute
-        self.requires("dependency/0.8.1")
-
     def validate(self):
-        # validate the minimum cpp standard supported. For C++ projects only
         if self.settings.compiler.cppstd:
             check_min_cppstd(self, self._min_cppstd)
-        minimum_version = self._compilers_minimum_version.get(str(self.settings.compiler), False)
-        if minimum_version and Version(self.settings.compiler.version) < minimum_version:
-            raise ConanInvalidConfiguration(
-                f"{self.ref} requires C++{self._min_cppstd}, which your compiler does not support."
-            )
-        # in case it does not work in another configuration, it should validated here too
-        if is_msvc(self) and self.options.shared:
-            raise ConanInvalidConfiguration(f"{self.ref} can not be built as shared on Visual Studio and msvc.")
-
-    # if another tool than the compiler or CMake is required to build the project (pkgconf, bison, flex etc)
-    def build_requirements(self):
-        self.tool_requires("tool/x.y.z")
+        if self.settings.os not in ["Linux", "iOS", "watchOS", "tvOS", "visionOS", "Macos", "Android", "FreeBSD", "SunOS", "AIX", "Arduino", "Neutrino", "baremetal", "VxWorks"]:
+            raise ConanInvalidConfiguration(f"{self.ref} is not supported on {self.settings.os}.")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
+        self._patch_sources()
 
     def generate(self):
-        # BUILD_SHARED_LIBS and POSITION_INDEPENDENT_CODE are automatically parsed when self.options.shared or self.options.fPIC exist
         tc = CMakeToolchain(self)
-        # Boolean values are preferred instead of "ON"/"OFF"
-        tc.variables["PACKAGE_CUSTOM_DEFINITION"] = True
-        if is_msvc(self):
-            # don't use self.settings.compiler.runtime
-            tc.variables["USE_MSVC_RUNTIME_LIBRARY_DLL"] = not is_msvc_static_runtime(self)
-        # deps_cpp_info, deps_env_info and deps_user_info are no longer used
-        if self.dependencies["dependency"].options.foobar:
-            tc.variables["DEPENDENCY_LIBPATH"] = self.dependencies["dependency"].cpp_info.libdirs
-        # cache_variables should be used sparingly, example setting cmake policies
-        tc.cache_variables["CMAKE_POLICY_DEFAULT_CMP0077"] = "NEW"
+        tc.variables["pfs_BUILD_ASAN"] = self.options.asan
+        tc.variables["pfs_BUILD_COVERAGE"] = self.options.coverage
+        tc.variables["pfs_BUILD_SAMPLES"] = self.options.build_samples
+        tc.variables["pfs_BUILD_TESTS"] = self.options.build_tests
         tc.generate()
-        # In case there are dependencies listed on requirements, CMakeDeps should be used
+
         tc = CMakeDeps(self)
         tc.generate()
-        # In case there are dependencies listed on build_requirements, VirtualBuildEnv should be used
-        tc = VirtualBuildEnv(self)
-        tc.generate(scope="build")
 
     def _patch_sources(self):
         apply_conandata_patches(self)
-        # remove bundled xxhash
-        rm(self, "whateer.*", os.path.join(self.source_folder, "lib"))
-        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"), "...", "")
 
     def build(self):
-        self._patch_sources()  # It can be apply_conandata_patches(self) only in case no more patches are needed
+        if self.settings.os == "Android":
+            replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+                            "target_link_libraries (unittest PRIVATE pfs)",
+                            "target_link_libraries (unittest PRIVATE pfs -llog)")
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
@@ -135,7 +98,6 @@ class PackageConan(ConanFile):
         cmake = CMake(self)
         cmake.install()
 
-        # some files extensions and folders are not allowed. Please, read the FAQs to get informed.
         rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
         rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
         rmdir(self, os.path.join(self.package_folder, "share"))
@@ -144,26 +106,12 @@ class PackageConan(ConanFile):
         rm(self, "*.pdb", os.path.join(self.package_folder, "bin"))
 
     def package_info(self):
-        self.cpp_info.libs = ["package_lib"]
+        self.cpp_info.libs = ["pfs"]
 
-        # if package has an official FindPACKAGE.cmake listed in https://cmake.org/cmake/help/latest/manual/cmake-modules.7.html#find-modules
-        # examples: bzip2, freetype, gdal, icu, libcurl, libjpeg, libpng, libtiff, openssl, sqlite3, zlib...
-        self.cpp_info.set_property("cmake_module_file_name", "PACKAGE")
-        self.cpp_info.set_property("cmake_module_target_name", "PACKAGE::PACKAGE")
-        # if package provides a CMake config file (package-config.cmake or packageConfig.cmake, with package::package target, usually installed in <prefix>/lib/cmake/<package>/)
-        self.cpp_info.set_property("cmake_file_name", "package")
-        self.cpp_info.set_property("cmake_target_name", "package::package")
-        # if package provides a pkgconfig file (package.pc, usually installed in <prefix>/lib/pkgconfig/)
-        self.cpp_info.set_property("pkg_config_name", "package")
-
+        # if self.settings.os == "Android":
+        #     self.cpp_info.system_libs.append("log")
         # If they are needed on Linux, m, pthread and dl are usually needed on FreeBSD too
-        if self.settings.os in ["Linux", "FreeBSD"]:
-            self.cpp_info.system_libs.append("m")
-            self.cpp_info.system_libs.append("pthread")
-            self.cpp_info.system_libs.append("dl")
-
-        # TODO: to remove in conan v2 once cmake_find_package_* generators removed
-        self.cpp_info.filenames["cmake_find_package"] = "PACKAGE"
-        self.cpp_info.filenames["cmake_find_package_multi"] = "package"
-        self.cpp_info.names["cmake_find_package"] = "PACKAGE"
-        self.cpp_info.names["cmake_find_package_multi"] = "package"
+        # if self.settings.os in ["Linux", "FreeBSD"]:
+        #     self.cpp_info.system_libs.append("m")
+        #     self.cpp_info.system_libs.append("pthread")
+        #     self.cpp_info.system_libs.append("dl")
